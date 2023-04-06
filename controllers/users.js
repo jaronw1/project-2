692c9d129cc67427be5b7051d97e9ab719005302
jaronw1 <jaronwong6@gmail.com>
2023-04-06 12:29:14 -0700

    going

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+
+//GET USER/ signup
+
+//post
+//get
+//post
+//get
+//get
+
+
+
+
+
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 8000
+app.set('view engine, ejs')
+
+app.use(express.urlencoded({ extended: false}))
+
+app.get('/', (req, res) => {
+    res.send('welcome to the auth api!')
+})
+
+app.use('users'), require('./controllers/users.js')
+
+
+app.listen(PORT, () => {
+    console.log(`authenticating user on port ${PORT}`)
+})
