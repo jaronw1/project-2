139aed08112ac3ed0648b829e2e82c79f307c21a
jaronw1 <jaronwong6@gmail.com>
2023-04-13 14:08:46 -0700

    preps for dep

--- test-db.js
+++ test-db.js
@@ -1,16 +1,2 @@
 const db = require('./models');
 
-const crud = async() => {
-    try {
-        //CREATE
-        const newDino = await db.dino.create({
-            name: 'Mr. T Rex',
-            type: "a-type dinosaur",
-        })
-
-        const newCreature = await db.creature.create({
-
-            type: "squirrel",
-            img_url: "squirrel.com"
-        })
-    }}
